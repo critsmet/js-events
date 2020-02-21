57400c523f18f04b2ce42ca848a48e7fdcf1081b
Chris Metzger <chrismetzgr@gmail.com>
2020-02-21 12:01:34 -0600

    upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# JS Events
+
+* Reminders to touch back on:
+  * What are closures
+  * Uses of console.log
+  * Example of debugger
+
+## Key Topics:
+
+* Event Listeners and Callback Functions
+  * What they are and when we can add them
+
+* Types of JS events
+  * Events on the window, events on elements
+
+* Event Bubbling & Capturing
+  * Difference between bubbling and capturing
+  * Stopping propagation
+
+* Using EventListeners and Callbacks to make changes to the DOM
+  * Event listeners pass an event object to the callback function
+  * Practice appending correct information to the DOM
+  * How and when to remove event listeners -- more on this next week
---
+++ index.js
@@ -0,0 +1,134 @@
+// - - - - - - - - - --
+//QUICK DEMO OF CLOSURE
+// - - - - - - - - - --
+
+// function outerFunction(){
+//   const secretWord = "Bubbles"
+//
+//   function saySecretWord(){
+//     console.log(secretWord)
+//   }
+//
+//   return saySecretWord
+// }
+//
+// const secretWordFunction = outerFunction()
+//
+// secretWordFunction()
+
+//CLOSURE: Any situation in which we are aware that we are defining a function within the scope of another function, and we use that to our advantage/apply that
+
+// - - - - - - - - - --
+//EVENT LISTENERS
+// - - - - - - - - - --
+
+// body.addEventListener('click', function(){console.log("Captured: body!")}, {capture: true})
+// addEmojiArea.addEventListener('click', function(){console.log("Captured: Div!")}, {capture: true})
+// button.addEventListener('click', function(){console.log("Captured: Button!")}, true)
+//
+// body.addEventListener('click', function(){console.log("Bubbling: body!")})
+// addEmojiArea.addEventListener('click', function(){console.log("Bubbling: Div!")})
+// button.addEventListener('click', function(){console.log("Bubbling: Button!")})
+//addEventListener takes two arguments, the first is a string of the event type,
+//and the second is a callback function that basically determines which action to take
+//the callback function must be a function reference NOT a function execution
+
+
+// function handleClick(){
+//   console.log("We clicked the button")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//As a developer I want...
+
+//a form to appear in place of the 'Add Emoji' button when the 'Add Emoji' button is clicked
+//an emoji can be entered into the input, submitted, and added to the "Emoji Bank"
+//an alert saying "Emoji Submitted!" whenever an emoji is submitted
+//the form turns back into the 'Add Emoji' button once an emoji has been successfully submitted
+//an emoji I click on turns into '🤪'
+
+//CHYRELLE
+
+const button = document.getElementById('add-button')
+const addEmojiArea = document.getElementById('add-emoji-area')
+const emojiBank = document.getElementById("emoji-bank")
+
+addEmojiArea.addEventListener('click', handleAreaClick)
+
+function handleAreaClick(event){
+  const form = `
+    <form>
+      <input id="emoji-input" type="text"/>
+      <input id="submit" type="submit" value="Submit Emoji!" />
+    </form>
+  `
+  if (event.target.id == "add-button") {
+    console.log("This is the change form button")
+    addEmojiArea.innerHTML = form
+  }
+  else if (event.target.id == "submit") {
+    event.preventDefault()
+    const emojiInput = document.getElementById('emoji-input')
+    const emoji = emojiInput.value
+
+    emojiBank.innerHTML += `
+      <li class="emoji">${emoji}</li>
+    `
+    //we already have the emoji in the 'emoji' variable, and now we want to put it in as an LI in the UL element
+    //so what should we do first?
+
+    //run the function that we haven't written yet
+    console.log("This is the submit emoji button")
+    const addButton = `<button id="add-button">Add Emoji</button>`
+    addEmojiArea.innerHTML = addButton
+  }
+
+}
+
+//1. we want to replace the HTML inside of the add-emoji-area div with a form, not a button
+//We'll do this by...appending a form to addEmojiArea and remove the button tag
+
+//2. we want to take the emoji that's been placed in the input ON SUBMIT, append it to the UL as an LI, and reset the form back to the add emoji button, and add an alert.
+
+//
+
+// const form = document.createElement('form')
+// const input = document.createElement('input')
+// form.appendChild(input)
+
+// button.addEventListener('click', addForm)
+
+
+
+//SPA: Single page applications: there are no refreshes, all DOM changes are made with JS
+
+
+
+
+
+
+
+
+
+
+
+//
